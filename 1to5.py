13ad92db15614270611ac119a2d34fc27f3a166a
KiikiTinna <68912227+KiikiTinna@users.noreply.github.com>
2021-04-06 15:04:13 +0200

    Update and rename 1.py to 1to5.py

---
+++ 1to5.py
@@ -0,0 +1,38 @@
+#Question 1: What will this script produce?
+a1 = 1
+a1 = 2
+a1 = 3
+print(a1)
+
+#Question 2: What will this script produce?
+a2 = 2
+a2 = 4
+a2 = 6
+print(a2 + a2 + a2)
+
+
+#Question 3: What's wrong with the following script?
+a = 1
+_a = 2
+_a2 = 3
+2a = 4
+
+#Question 4: Executing the code will throw an error. Can you explain why?
+a = 1
+b = 2
+print(a == b)
+print(b == c)
+
+#Question 5: Fix the last line so that it outputs the sum of 1 and 2. Please do not change the first two lines, only the last one.
+a5 = "1"
+b5 = 2
+print(a5 + b5)
+
+#Hint 3: There are certain naming rules you need to follow when creating variables.
+#Hint 4: Operator == is a comparison operator.
+
+
+#Answer
+#a1: 3
+#a2: 18
+#a3: 
