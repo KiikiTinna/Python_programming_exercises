904e9e433a66f9fb1175724b48f4eecd481c5b71
KiikiTinna <68912227+KiikiTinna@users.noreply.github.com>
2021-04-06 17:21:04 +0200

    Update and rename 11.py to 11to15.py

---
+++ 11to15.py
@@ -0,0 +1,35 @@
+#Question 11:Create a script that generates a list of numbers from 1 to 20. Do not create a list manually.
+
+
+#Question 12:Create a script that generates a list whose items are products of the original list items multiplied by 10
+
+
+#Question 13:Create a script that converts all items of the range to strings
+my_range = range(1, 21)
+
+
+#Question 14: Write a script that remove duplicates from a list
+a = ["1", 1, "1", 2]
+
+
+#Question 15: Create a dictionary of two keys, a and b and two values 1 and 2 for keys a and b respectively.
+
+
+
+
+
+#Answers:
+#Q11: #my_range = range(1, 21) 
+      #print(list(my_range))
+
+#Q12: #my_range = range(1, 21)
+      #print([10 * x for x in my_range])
+  
+#Q13: #print(map(str, my_range))
+      #for i in map:
+          #print(i)
+    
+#Q14: #a = list(set(a))
+      #print(a)
+
+#Q15: # d = {"a": 1, "b": 2}
