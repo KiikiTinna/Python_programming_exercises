9963fb2fe0194487438e6b161ab5ee6bdd8217f3
KiikiTinna <68912227+KiikiTinna@users.noreply.github.com>
2021-04-06 17:03:40 +0200

    Update and rename 6.py to 6to10.py

---
+++ 6to10.py
@@ -0,0 +1,25 @@
+#Question 6: Complete the script so that it prints out a list containing letters d, e, f.
+letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]
+
+#Question 7:Complete the script so that it prints out a list containing the first three elements of list letters
+letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]
+
+#Question 8: Complete the script so that it prints out the letter i using negative indexing
+letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]
+
+#Question 9: Complete the script so that it prints out a list containing the last three elements of list letters
+letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]
+
+#Question 10: Complete the script so that it prints out a list containing letters a, c, e, g, and i so the at a step of two
+letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]
+
+
+
+
+
+#Answers:
+#Q6: print(letters[3:6])
+#Q7: print(letters[:3])
+#Q8: print(letters[-2])
+#Q9: print(letters[-3:])
+#Q10: print(letters[::2])
