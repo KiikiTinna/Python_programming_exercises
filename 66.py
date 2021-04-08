13c2a524d2f6bec3e869252510e486d78a27de8a
KiikiTinna <68912227+KiikiTinna@users.noreply.github.com>
2021-04-08 16:34:30 +0200

    Update 66.py

--- 66.py
+++ 66.py
@@ -1,7 +1,20 @@
-#Create a function that takes a word from user and translates it using a dictionary of three words
+# Questiona Create a function that takes a word from user and translates it using a dictionary of three words, 
+#returning a message when the word is not in the dict,
+# considering user may enter different letter cases
 d = dict(weather = "clima", earth = "terra", rain = "chuva")
-def vocabulary(word):
-    return d[word]
 
-word = input("Enter word: ")
-print(vocabulary(word))
+
+
+
+
+
+#Answers
+
+#def vocabulary(word):
+#    try:
+#        return d[word]
+#    except KeyError:
+#        return "That word does not exist."
+
+#word = input("Enter word: ").lower()
+#print(vocabulary(word))
